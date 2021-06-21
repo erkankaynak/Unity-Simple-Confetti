321abafce6a1473e0d6b7e4cc861e38fe8efbfc9
erkan kaynak <erkan.kaynak.001@gmail.com>
2021-06-21 10:21:19 +0300

    Add files via upload

---
+++ Assets/Scripts/confetti.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class confetti :MonoBehaviour
+    {
+        public Sprite[] sprites;
+
+        [HideInInspector] public Color color;
+        [HideInInspector] public float speed;
+        [HideInInspector] public float destroyCoordinate;
+
+        private int frame;
+        private SpriteRenderer spriteRenderer;
+
+        private void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            frame = Random.Range(0, 13);
+            transform.Rotate(0, 0, Random.Range(75,105));
+            spriteRenderer.sprite = sprites[frame];
+            spriteRenderer.color = color;
+
+            StartCoroutine(Move());
+            StartCoroutine(Animate());
+        }
+
+
+        private IEnumerator Move()
+        {
+            var v = transform.position;
+
+            while (transform.position.y > destroyCoordinate)
+            {
+                //v.y = transform.position.y;
+                v.y = v.y - .5f * Time.deltaTime;
+
+                //transform.Translate(v.x, v.y, 0);
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+                yield return null;
+            }
+         
+            Destroy(gameObject);
+        }
+
+        private IEnumerator Animate()
+        {
+            var waitTime = new WaitForSeconds(.2f);
+            while (true)
+            {
+                frame++;
+                if (frame == sprites.Length) frame = 0;
+                spriteRenderer.sprite = sprites[frame];
+
+                yield return waitTime;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/windowConfetti.cs
@@ -0,0 +1,57 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windowConfetti : MonoBehaviour
+{
+    public GameObject ConfettiPrefab;
+
+    [Space]
+    public float spawnVerticalCoordinate = 5f; // Vertical coordinate of where the confetties will spawn
+    public float destroyVerticalCoordinate = -5f; // Vertical coordinate of where the confetties will destroy
+
+    [Space]
+    public float spawnAreaSize = 5f; //  Width of horizontal area where confetties will spawn
+    public float minSpeed = 1f;
+    public float maxSpeed = 1.5f;
+    public int confettiSpawnCount = 5;
+
+    [Space]
+    public Color[] colors;
+
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+
+    IEnumerator SpawnRoutine()
+    {
+        var waitTime = new WaitForSeconds(.3f);
+        while (true)
+        {
+            for (int i=1; i <= confettiSpawnCount; i++)
+            {
+                SpawnConfetti();
+            }
+            yield return waitTime;
+        }
+    }
+
+    private confetti SpawnConfetti()
+    {
+        var spawnX = Random.Range(-spawnAreaSize / 2, spawnAreaSize / 2);
+        var spawnPos = new Vector3(spawnX, spawnVerticalCoordinate);
+
+        var confettiObject = Instantiate(ConfettiPrefab, spawnPos,Quaternion.identity,transform);
+        var confetti = confettiObject.GetComponent<confetti>();
+        confetti.color = colors[Random.Range(0, colors.Length)];
+        confetti.speed = Random.Range(minSpeed, maxSpeed);
+        confetti.destroyCoordinate = destroyVerticalCoordinate;
+
+        return confetti;
+    }
+
+   
+}
